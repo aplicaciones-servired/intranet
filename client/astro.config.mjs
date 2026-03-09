ff9ba0ca3186fb2de4f27d7d5106490f91d15243
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2026-03-09 11:48:12 -0500

    agregar resolver DNS de Docker a nginx

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -10,8 +10,11 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react(), clerk({ localization: esES, signInUrl: '/sign-in', signUpUrl: '/sign-in' })],
+  server: {
+    host: true,
+    port: 4321,
+  },
   adapter: node({ mode: "standalone" }),
   output: "server",
 });
